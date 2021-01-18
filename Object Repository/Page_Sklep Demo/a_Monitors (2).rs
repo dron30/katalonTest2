c848a4734d30a868d411bdca27e689852d1eaa71
dron30 <58118715+dron30@users.noreply.github.com>
2021-01-18 17:53:08 +0100

    Add files via upload

--- Object Repository/Page_Sklep Demo/a_Monitors (2).rs
+++ Object Repository/Page_Sklep Demo/a_Monitors (2).rs
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>a_Cameras</name>
+   <name>a_Monitors (2)</name>
    <tag></tag>
-   <elementGuidId>a7bfd8fd-67a7-4e33-b223-928a858b379a</elementGuidId>
+   <elementGuidId>bb47603e-03ca-48aa-b938-8b7458fe4c0e</elementGuidId>
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>//nav[@id='menu']/div[2]/ul/li[7]/a</value>
+         <value>//nav[@id='menu']/div[2]/ul/li[3]/div/div/ul/li[2]/a</value>
       </entry>
       <entry>
          <key>CSS</key>
@@ -28,83 +28,83 @@
       <matchCondition>equals</matchCondition>
       <name>href</name>
       <type>Main</type>
-      <value>http://www.sklep.bmsystem.pl/index.php?route=product/category&amp;path=33</value>
+      <value>http://www.sklep.bmsystem.pl/index.php?route=product/category&amp;path=25_28</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
-      <value>Cameras</value>
+      <value>Monitors (2)</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>id(&quot;menu&quot;)/div[@class=&quot;collapse navbar-collapse navbar-ex1-collapse&quot;]/ul[@class=&quot;nav navbar-nav&quot;]/li[7]/a[1]</value>
+      <value>id(&quot;menu&quot;)/div[@class=&quot;collapse navbar-collapse navbar-ex1-collapse&quot;]/ul[@class=&quot;nav navbar-nav&quot;]/li[@class=&quot;dropdown&quot;]/div[@class=&quot;dropdown-menu&quot;]/div[@class=&quot;dropdown-inner&quot;]/ul[@class=&quot;list-unstyled&quot;]/li[2]/a[1]</value>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:idRelative</name>
       <type>Main</type>
-      <value>//nav[@id='menu']/div[2]/ul/li[7]/a</value>
+      <value>//nav[@id='menu']/div[2]/ul/li[3]/div/div/ul/li[2]/a</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:link</name>
       <type>Main</type>
-      <value>(//a[contains(text(),'Cameras')])[2]</value>
+      <value>//a[contains(text(),'Monitors (2)')]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='Phones &amp; PDAs'])[1]/following::a[1]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Mice and Trackballs (0)'])[1]/following::a[1]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='Software'])[1]/following::a[2]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Components'])[1]/following::a[2]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='MP3 Players'])[1]/preceding::a[1]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Printers (0)'])[1]/preceding::a[1]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='test 11 (0)'])[1]/preceding::a[2]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Scanners (0)'])[1]/preceding::a[2]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>//*/text()[normalize-space(.)='Cameras']/parent::*</value>
+      <value>//*/text()[normalize-space(.)='Monitors (2)']/parent::*</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:href</name>
       <type>Main</type>
-      <value>//a[@href='http://www.sklep.bmsystem.pl/index.php?route=product/category&amp;path=33']</value>
+      <value>//a[@href='http://www.sklep.bmsystem.pl/index.php?route=product/category&amp;path=25_28']</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:position</name>
       <type>Main</type>
-      <value>//li[7]/a</value>
+      <value>//li[3]/div/div/ul/li[2]/a</value>
    </webElementXpaths>
 </WebElementEntity>
